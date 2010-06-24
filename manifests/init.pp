e3826c1409381aff4158d6c3364e3a90c12ec27c
Garrett Honeycutt <github@garretthoneycutt.com>
2010-06-24 14:32:43 -0700

    added license - GPLv2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,14 @@
+# Class: dnsclient
 #
-# DNS client module
+# This module manages /etc/resolv.conf
 #
-# Generate resolv.conf
+# Requires: $nameservers and $dnssearchpath which should be set in the site manifest
 #
-# $nameservers and $dnssearchpath are specified in the site manifest
+# Sample Usage: included in generic class that applies to all systems
 #
 class dnsclient {
 
     file { "/etc/resolv.conf": 
         content => template("dnsclient/resolv.conf.erb"),
-    }
+    } # file
 } # class dnsclient
